a7eec067747f73d6f200ebfad92fc1e9cbb7bd29
eukaryote <eukaryote31@users.noreply.github.com>
2018-01-13 12:31:50 -0700

    Use backslash-escape newlines instead

--- db/database.py
+++ db/database.py
@@ -7,32 +7,36 @@ class FaucetDB():
         self.db = records.Database(connection)
 
     def setup(self):
-        self.db.query("CREATE TABLE 'transactions' ("
-                      "'timestamp' BIGINT,"
-                      "'tailtx' CHAR(81),"
-                      "'confirmed' BOOL"
-                      ")")
+        self.db.query("CREATE TABLE 'transactions' ( \
+                      'id' INT NOT NULL AUTO_INCREMENT \
+                      'timestamp' DECIMAL(12, 6), \
+                      'tailtx' CHAR(81), \
+                      'confirmed' BOOLEAN \
+                      )")
 
-        self.db.query("CREATE TABLE 'seeds' ("
-                      "'seed' CHAR(81),"
-                      "'balance' BIGINT,"
-                      "'timescanned' BIGINT,"
-                      "'stale' BOOL"
-                      ")")
+        self.db.query("CREATE TABLE 'seeds' ( \
+                      'id' INT NOT NULL AUTO_INCREMENT \
+                      'seed' CHAR(81), \
+                      'balance' BIGINT, \
+                      'timescanned' BIGINT, \
+                      'stale' BOOLEAN \
+                      )")
 
-        self.db.query("CREATE TABLE 'txs_nopow' ("
-                      "'timestamp' BIGINT,"
-                      "'trytes' CHAR(2673),"
-                      "'bundle' CHAR(81),"
-                      "'seed' CHAR(81),"
-                      "'tasktime' BIGINT,"
-                      ")")
+        self.db.query("CREATE TABLE 'txs_queue' ( \
+                      'id' INT NOT NULL AUTO_INCREMENT \
+                      'timestamp' DECIMAL(12, 6), \
+                      'trytes' CHAR(2673), \
+                      'bundle' CHAR(81), \
+                      'seed' CHAR(81), \
+                      'tasktime' BIGINT, \
+                      'pow' BOOLEAN \
+                      )")
 
-        self.db.query("CREATE TABLE 'payouts' ("
-                      "'timestamp' BIGINT,"
-                      "'address' CHAR(81),"
-                      "'amount' BIGINT,"
-                      ")")
+        self.db.query("CREATE TABLE 'payouts' ( \
+                      'timestamp' DECIMAL(12, 6), \
+                      'address' CHAR(81), \
+                      'amount' BIGINT \
+                      )")
 
     def payout(self, address, amount):
         self.db.query("INSERT INTO 'payouts' ('timestamp','address','amount') \
